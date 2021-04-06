74d7cb7cf37e156cc235e498a0b8a9fef25fc3c3
Élie Meignan <contact@eliepse.fr>
2021-04-06 23:28:21 +0200

    change: file structure according to framework

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Psr\Http\Message\ResponseInterface as Response;
+use Eliepse\Argile\Http\Responses\ViewResponse;
+use Psr\Http\Message\ResponseInterface;
 
 class WelcomeController
 {
-	public function __invoke(): Response
+	public function __invoke(): ResponseInterface
 	{
-		return view("welcome", ["page" => "welcome"]);
+		return new ViewResponse("welcome", ["page" => "welcome"]);
 	}
 }
